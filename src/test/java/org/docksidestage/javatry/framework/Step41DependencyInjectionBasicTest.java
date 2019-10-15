e744913a21cc647a2ddda19d5c6666e4008448d8
jflute <jflute@jflute-nyabara>
2019-10-16 02:35:50 +0900

    make Step41DependencyInjectionBasicTest fisrt

---
+++ src/test/java/org/docksidestage/javatry/framework/Step41DependencyInjectionBasicTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.javatry.framework;
+
+import org.docksidestage.unit.PlainTestCase;
+
+/**
+ * The test of Dependency Injection (DI) as basic level. <br>
+ * Show answer by log() or write answer on comment for question of javadoc.
+ * @author jflute
+ * @author your_name_here
+ */
+public class Step41DependencyInjectionBasicTest extends PlainTestCase {
+
+    // ===================================================================================
+    //                                                                        Precondition
+    //                                                                        ============
+    /**
+     * Search "Dependency Injection" by internet and learn it in thirty minutes. (study only) <br>
+     * ("Dependency Injection" をインターネットで検索して、30分ほど学んでみましょう。(勉強のみ))
+     */
+    public void test_whatis_DependencyInjection() {
+        // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+        // What is Dependency Injection?
+        // - - - - - (your answer?)
+        //
+        //
+        //
+        // _/_/_/_/_/_/_/_/_/_/
+    }
+
+    // ===================================================================================
+    //                                                                 Non DI Code Reading
+    //                                                                 ===================
+    /**
+     * What is the difference between NonDiDirectFirstAction and NonDiDirectSecondAction? <br>
+     * (NonDiDirectFirstAction と NonDiDirectSecondAction の違いは？)
+     */
+    public void test_nondi_difference_between_first_and_second() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    /**
+     * What is the difference between NonDiDirectSecondAction and NonDiFactoryMethodAction? <br>
+     * (NonDiDirectSecondAction と NonDiFactoryMethodAction の違いは？)
+     */
+    public void test_nondi_difference_between_second_and_FactoryMethod() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    /**
+     * What is the difference between NonDiFactoryMethodAction and NonDiIndividualFactoryAction? <br>
+     * (NonDiFactoryMethodAction と NonDiIndividualFactoryAction の違いは？)
+     */
+    public void test_nondi_difference_between_FactoryMethod_and_IndividualFactory() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    // ===================================================================================
+    //                                                               Using DI Code Reading
+    //                                                               =====================
+    /**
+     * What is the difference between UsingDiAccessorAction and UsingDiAnnotationAction? <br>
+     * (UsingDiAccessorAction と UsingDiAnnotationAction の違いは？)
+     */
+    public void test_usingdi_difference_between_Accessor_and_Annotation() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    /**
+     * What is the difference between UsingDiAnnotationAction and UsingDiDelegatingAction? <br>
+     * (UsingDiAnnotationAction と UsingDiDelegatingAction の違いは？)
+     */
+    public void test_usingdi_difference_between_Annotation_and_Delegating() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    // ===================================================================================
+    //                                                           Execute like WebFramework
+    //                                                           =========================
+    /**
+     * Execute accessor and annotation and delegating actions by UsingDiWebFrameworkProcess and why not NullPointerException? <br>
+     * (accessor, annotation, delegating の Action を UsingDiWebFrameworkProcess 経由で実行してみましょう。そして、なぜ NullPointerException にならないのか？)
+     */
+    public void test_usingdi_UsingDiWebFrameworkProcess() {
+        // your answer? => 
+        // and execution code here
+    }
+
+    /**
+     * What is concrete class of instance variable "animal" of UsingDiAnnotationAction? (when you execute by UsingDiWebFrameworkProcess) <br>
+     * (UsingDiAnnotationAction のインスタンス変数 "animal" の実体クラスは？ (UsingDiWebFrameworkProcessで実行した時))
+     */
+    public void test_usingdi_whatis_animal() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    // ===================================================================================
+    //                                                                        DI Container
+    //                                                                        ============
+    /**
+     * What is DI container? <br>
+     * (DIコンテナとは？)
+     */
+    public void test_whatis_DIContainer() {
+        // your answer? => 
+        // and your confirmation code here freely
+    }
+
+    // ===================================================================================
+    //                                                                           Good Luck
+    //                                                                           =========
+    /**
+     * What is class or file of DI settings that defines MemberBhv class as DI component in the following Lasta Di application? <br>
+     * (以下のLasta DiアプリケーションでMemberBhvクラスをDIコンポーネントとして定義しているDI設定クラスもしくはファイルは？) <br>
+     * 
+     * https://github.com/lastaflute/lastaflute-example-harbor
+     */
+    public void test_zone_search_component_on_LastaDi() {
+        // your answer? => 
+    }
+
+    /**
+     * What is class or file of DI settings that defines MemberBhv class as DI component in the following Spring application? <br>
+     * (以下のSpringアプリケーションでMemberBhvクラスをDIコンポーネントとして定義しているDI設定クラスもしくはファイルは？) <br>
+     * 
+     * https://github.com/dbflute-example/dbflute-example-on-springboot
+     */
+    public void test_zone_search_component_on_Spring() {
+        // your answer? => 
+    }
+}
