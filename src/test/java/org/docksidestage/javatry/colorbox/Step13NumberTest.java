42b5902a3ed44af8500f9ca79c7a0ce0c67a242f
jflute <jflute@jflute-nyabara>
2019-10-18 21:39:59 +0900

    add "between 0 and 54 (includes)" of step13

--- src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
@@ -29,7 +29,7 @@ public class Step13NumberTest extends PlainTestCase {
     //                                                                               Basic
     //                                                                               =====
     /**
-     * How many integer-type values in color-boxes are between 0 and 54? <br>
+     * How many integer-type values in color-boxes are between 0 and 54 (includes)? <br>
      * (カラーボックの中に入っているInteger型で、0から54までの値は何個ある？)
      */
     public void test_countZeroToFiftyFour_IntegerOnly() {
