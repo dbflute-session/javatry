5aa283696eda3eaaa5e98308001d30632ff4701b
certz <batzaya.noroverdene@bizreach.co.jp>
2020-04-19 22:27:45 +0900

    fix: 要らなくなったメソッド消し

--- src/test/java/org/docksidestage/javatry/debug/Step22SearcherTest.java
+++ src/test/java/org/docksidestage/javatry/debug/Step22SearcherTest.java
@@ -1,7 +1,6 @@
 package org.docksidestage.javatry.debug;
 
 import org.docksidestage.bizfw.debug.Word;
-import org.docksidestage.bizfw.debug.searcher.BinarySearcher;
 import org.docksidestage.bizfw.debug.searcher.IteratorSearcher;
 import org.docksidestage.bizfw.debug.searcher.LinearSearcher;
 import org.docksidestage.unit.PlainTestCase;
@@ -14,19 +13,6 @@ import org.junit.Assert;
  * @author zaya
  */
 public class Step22SearcherTest extends PlainTestCase {
-
-    public void test_binarySearch_found() {
-        // act
-        Word found = new BinarySearcher().search("私");
-
-        // assert
-        Assert.assertEquals("日本語", found.getLanguage().name);
-        Assert.assertEquals("私", found.getWord());
-    }
-
-    public void test_binarySearch_notFound() {
-        assertException(IllegalArgumentException.class, () -> new BinarySearcher().search("君"));
-    }
 
     public void test_iteratorSearch_found() {
         // act
