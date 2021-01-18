44dbc311e7d12a562e9faa58feb8d18c9efa5fcf
jflute <jflute@jflute-nyabara>
2021-01-18 16:23:01 +0900

    improve: small refactoring and add copyright

--- src/main/java/org/docksidestage/bizfw/debug/sorter/BubbleSorter.java
+++ src/main/java/org/docksidestage/bizfw/debug/sorter/BubbleSorter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.docksidestage.bizfw.debug.sorter;
 
 import java.util.List;
@@ -6,8 +21,10 @@ import org.docksidestage.bizfw.debug.Word;
 
 /**
  * @author zaya
+ * @author jflute
  */
 public class BubbleSorter implements Sorter<Word> {
+
     private List<Word> words;
 
     public BubbleSorter() {
